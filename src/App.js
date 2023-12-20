6fc0c0af0c16a7818c432fc3ad43748846c3607d
dmbartender91 <jje84@joesdesigns.com>
2023-12-19 23:50:23 -0500

    Simple coming soon messa

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [messageText, setMessageText] = useState("");
+  const [subtext, setSubtext] = useState("coming soon");
+  useEffect(()=>{
+    setMessageText("joeegan.dev");
+  },[]);
+  
+  return (
+    <div className='App full-height'>
+      <div className='container-fluid bg-dark'>
+          <div className='row'>
+            <div className='col-md-2'></div>
+            <div class="col-md-8 ">
+                <div className='mt-5 display-1 text-center'>
+                  {messageText}
+                </div>
+                <div className='mt-5 display-5 text-center pb-5'>
+                  {subtext}
+
+              </div>
+            </div>
+            <div className='col-md-2'></div>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
