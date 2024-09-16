eb4017e6e56fdcc6a53f04aaf535ce5601036e2b
dmbartender91 <jje84@joesdesigns.com>
2024-09-15 23:29:02 -0400

    Import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 
 function App() {
